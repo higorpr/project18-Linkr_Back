1f95337fed0f3da4512f31191cc44b566bc468b8
ThaisFReis <reis.thaisf@gmail.com>
2023-01-08 20:05:14 -0300

    Sign up atualizado

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { login, logout } from "../controllers/authController.js";
-import { loginValidation } from "../middlewares/authMiddleware.js";
+import { login, logout, signUpControllers } from "../controllers/authController.js";
+import { loginValidation, signUpMiddlewares } from "../middlewares/authMiddleware.js";
 import { tokenValidation } from "../middlewares/tokenValidation.js";
 
 const authRouter = Router();
 
 authRouter.post("/login", loginValidation, login);
 authRouter.post("/logout", tokenValidation, logout);
+authRouter.post("/signup", signUpMiddlewares, signUpControllers);
 
 export default authRouter;

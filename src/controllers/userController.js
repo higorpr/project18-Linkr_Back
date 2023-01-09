f621f2dad414c3c7764af3cf1a2a64718ec6e680
Lucas Macedo <lucasamacedo@hotmail.com>
2023-01-09 15:28:21 -0300

    fix: query of page user post modified to atend the post on front

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,34 +1,74 @@
-import connection from "../database/db.js"
+import connection from "../database/db.js";
+import urlMetadata from "url-metadata";
 
+export async function getUserLinks(req, res) {
+	const { id } = req.params;
 
-export async function getUserLinks(req, res){
-    const {id} = req.params;
-
-    try{
-        const user = (await connection.query(`
+	try {
+		const user = (
+			await connection.query(
+				`
             SELECT * 
             FROM users 
             WHERE id = $1;
-        `,[id])).rows;
+        `,
+				[id]
+			)
+		).rows;
 
-        if(user.length === 0 ){
-            return res.status(404).send("User not found!");
-        }
+		if (user.length === 0) {
+			return res.status(404).send("User not found!");
+		}
 
-        const posts = (await connection.query(`
-            SELECT u.username, u.id, u.image, p.* 
-            FROM users u JOIN posts p
-                ON  u.id = p.user_id
-            WHERE p.user_id = $1 
+		const posts = (
+			await connection.query(
+				`
+            SELECT 
+                posts.*,
+                users.username as username,
+                users.image as image,
+                COUNT(liked_posts.user_id) as likes,
+				($1=posts.user_id) as "ownPost",
+                EXISTS (
+                    SELECT 
+                        true 
+                    FROM liked_posts 
+                    WHERE user_id=$1 
+                    AND post_id=posts.id
+                ) as "selfLike"
+            FROM posts 
+            LEFT JOIN users
+            ON users.id=posts.user_id
+            LEFT JOIN liked_posts
+            ON posts.id=liked_posts.post_id
+            WHERE posts.user_id = $1 
+            GROUP BY posts.id, users.username, users.image, users.id
             ORDER BY created_at DESC 
-            LIMIT 20;    
-        `, [id])).rows;
+            LIMIT 20;   
+        `,
+				[id]
+			)
+		).rows;
 
-        res.status(200).send(posts);
+		let i;
 
-    } catch(err){
-        console.log(err);
-        res.sendStatus(500);
-    }
+		for (i = 0; i < posts.length; i++) {
+			await urlMetadata(posts[i].link).then(
+				function (metadata) {
+					posts[i] = {
+						linkImage: metadata.image,
+						linkTitle: metadata.title,
+						linkDescription: metadata.description,
+						...posts[i],
+					};
+				},
+				function (error) {}
+			);
+		}
 
+		res.status(200).send(posts);
+	} catch (err) {
+		console.log(err);
+		res.sendStatus(500);
+	}
 }
